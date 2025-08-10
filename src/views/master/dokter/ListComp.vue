5cf2bb30b56d6111d9453d03f0256788c31ace76
abimayubierahman <abimayubierahman@gmail.com>
2025-08-10 23:25:51 +0700

    rev: master dokter

--- src/views/master/dokter/ListComp.vue
+++ src/views/master/dokter/ListComp.vue
@@ -1,20 +1,33 @@
 <template>
   <u-col gap="gap-1" class="w-full px-4 py-1">
     <u-view padding="0" flex1 class="w-full">
-      <u-row class="">
-        <div>
-          <div class="font-bold">{{ item?.nama_dokter }}</div>
-          <div class="text-xs text-gray-400">{{ useWaktuLaluReactive(item?.created_at) }}</div>
-        </div>
+      <u-row flex1 class="w-full">
+        <u-col gap = gap-1>
+          <u-grid cols=12>
+            <u-row class="col-span-1">
+              <span>Nama</span>
+            </u-row>
+            <u-row class="col-span-11">
+              <span>: {{ item?.nama_dokter }}</span>
+            </u-row>
+          </u-grid>
+          <u-grid cols=12>
+            <u-row class="col-span-1">
+              <span>Alamat</span>
+            </u-row>
+            <u-row class="col-span-11">
+              <span>: {{ item?.alamat }}</span>
+            </u-row>
+          </u-grid>
+        </u-col>
       </u-row>
-    <u-row class="">
+      <u-row right class="">
         <div>
-          <div>{{ item?.alamat }}</div>
-          <div class="text-xs text-gray-400">{{ useWaktuLaluReactive(item?.created_at) }}</div>
+          <div class="flex flex-col items-end gap-1 pt-2">
+            <u-btndrop label="Aksi" :items="['Edit', 'Hapus']" @select="val => handleSelect(val, item)" />
+            <span class="text-xs text-gray-400">{{ useWaktuLaluReactive(item?.created_at) }}</span>
+          </div>
         </div>
-      </u-row>
-      <u-row flex1 right class=" w-full">
-        <u-btndrop label="Aksi" :items="['Edit', 'Hapus']" @select="val => handleSelect(val, item)" />
       </u-row>
     </u-view>
   </u-col>
@@ -30,8 +43,7 @@ const emit = defineEmits(['edit', 'delete'])
 
 function handleSelect(x, item) {
   const val = x?.toLowerCase()
-  // console.log(val, item);
-  
+
   if (val === 'edit') {
     emit('edit', item)
   } else if (val === 'hapus') {
