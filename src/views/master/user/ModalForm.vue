3d70e593d29d35d371e9a290271edb9dd8df0db1
pigartpigaro <vichoirul@gmail.com>
2025-09-24 23:51:48 +0700

    rev: user tambah jabatan

--- src/views/master/user/ModalForm.vue
+++ src/views/master/user/ModalForm.vue
@@ -1,35 +1,31 @@
 <template>
-  <u-modal persistent :title="`Tambah ${title}`" @close="emit('close')" >
+  <u-modal persistent :title="`Tambah ${title}`" @close="emit('close')">
     <template #default>
       <u-col flex1 class="w-full p-4">
         <u-row flex1 class="w-full">
-          <u-input v-model="form.nama" label="Nama" 
-            :error="isError('nama')"
-            :error-message="errorMessage('nama')" 
-          />
+          <u-input v-model="form.nama" label="Nama" :error="isError('nama')" :error-message="errorMessage('nama')" />
         </u-row>
         <u-row flex1 class="w-full">
           <u-row flex1>
-            <u-input v-model="form.username" label="Username" 
-              :error="isError('username')"
-              :error-message="errorMessage('username')" 
-            />
+            <u-input v-model="form.username" label="Username" :error="isError('username')"
+              :error-message="errorMessage('username')" />
           </u-row>
           <u-row flex1>
-            <u-input v-model="form.email" label="Email" 
-              :error="isError('email')"
-              :error-message="errorMessage('email')" 
-            />
+            <u-input v-model="form.email" label="Email" :error="isError('email')"
+              :error-message="errorMessage('email')" />
           </u-row>
         </u-row>
         <u-row flex1 class="w-full">
           <u-row flex1>
-            <u-input v-model="form.hp" label="No. Hp" 
-              :error="isError('hp')"
-              :error-message="errorMessage('hp')" 
-            />
+            <u-input v-model="form.hp" label="No. Hp" :error="isError('hp')" :error-message="errorMessage('hp')" />
           </u-row>
           <u-row flex1>
+              <u-select label="Jabatan" v-model="form.kode_jabatan" :options="optionJabatans" 
+                :error="isError('kode_jabatan')" :error-message="errorMessage('kode_jabatan')" @update:modelValue="(val) => {
+                  console.log('val', val);
+
+                }" />
+           
             <!-- <u-input v-model="form.email" label="Email" 
               :error="isError('email')"
               :error-message="errorMessage('email')" 
@@ -37,25 +33,17 @@
           </u-row>
         </u-row>
         <u-row flex1 class="w-full">
-          <u-textarea
-            v-model="form.alamat"
-            label="alamat"
-            :error="isError('alamat')"
-            :error-message="errorMessage('alamat')"
-          />
+          <u-textarea v-model="form.alamat" label="alamat" :error="isError('alamat')"
+            :error-message="errorMessage('alamat')" />
         </u-row>
-         <u-row flex1 class="w-full">
+        <u-row flex1 class="w-full">
           <u-row flex1>
-            <u-input v-model="form.password" label="Buat Password" 
-              :error="isError('password')"
-              :error-message="errorMessage('password')" 
-            />
+            <u-input v-model="form.password" label="Buat Password" :error="isError('password')"
+              :error-message="errorMessage('password')" />
           </u-row>
           <u-row flex1>
-            <u-input v-model="form.password_confirmation" label="Password Confirmation" 
-              :error="isError('password_confirmation')"
-              :error-message="errorMessage('password_confirmation')" 
-            />
+            <u-input v-model="form.password_confirmation" label="Password Confirmation"
+              :error="isError('password_confirmation')" :error-message="errorMessage('password_confirmation')" />
           </u-row>
         </u-row>
       </u-col>
@@ -63,13 +51,14 @@
     <template #footer>
       <u-row flex1 class="w-full" right>
         <u-btn variant="secondary" label="Batal" @click="$emit('close')" />
-        <u-btn :loading="store.loadingSave" label="Simpan" type="button" @click.stop="handleSubmit"  />
+        <u-btn :loading="store.loadingSave" label="Simpan" type="button" @click.stop="handleSubmit" />
       </u-row>
     </template>
   </u-modal>
 </template>
 
 <script setup>
+import { useJabatanStore } from '@/stores/template/register'
 import { ref, onMounted, computed, watch } from 'vue'
 const props = defineProps({
   store: { type: Object, required: true },
@@ -85,6 +74,10 @@ const error = computed(() => {
   }
   return null
 })
+
+const masterJabatan = useJabatanStore()
+const optionJabatans = computed(() => masterJabatan?.items?.map(item => ({ label: item?.nama, value: item?.kode })) || [])
+
 
 const form = ref({
   nama: '',
@@ -105,13 +98,28 @@ function errorMessage(field) {
   return error.value?.[field]?.[0] ?? null
 }
 
+function init() {
+  const mode = props.mode
+  const item = props.store.item || {}
 
+  if (mode === 'add') {
+    form.value = Object.fromEntries(
+      Object.keys(form.value).map(k => [k, ''])
+    )
+  } else {
+    form.value = {
+      ...form.value,
+      ...Object.fromEntries(
+        Object.entries(item).filter(([key]) => !['created_at', 'updated_at'].includes(key))
+      ),
+      kode_jabatan: item.kode_jabatan
+    }
+  }
+}
 
 watch(
   () => ({ ...form.value }),
   (newForm, oldForm) => {
-    // console.log('🔥 watch form', newForm, oldForm);
-
     for (const key in newForm) {
       if (newForm[key] !== oldForm[key]) {
         props.store.clearFieldError(key)
@@ -122,32 +130,11 @@ watch(
 )
 
 
-
-
 function handleSubmit() {
   emit('save', form.value, props.mode)
 }
 
-function init() {
-  const mode = props.mode
-  if (mode === 'add') {
-    form.value = {
-      nama: ''
-    }
-  } else {
-    const item = props.store.item || {}
-    form.value = {
-      kode: item.kode || '',
-      nama: item.nama || ''
-    }
-  }
-
-  // console.log('init form', form.value);
-
-}
-
-onMounted(() => {
-  // console.log('Mounted Form', props.mode);
+onMounted( () => {
   init()
 })
 
